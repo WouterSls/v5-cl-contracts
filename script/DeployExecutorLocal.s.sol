738b75dc27f938316fb8f6cc08101ab860794012
wsels <phaerocode@hotmail.com>
2025-10-01 15:12:04 +0200

    chore: clean up scripts

--- script/DeployExecutorLocal.s.sol
+++ script/DeployExecutorLocal.s.sol
@@ -7,23 +7,20 @@ import {console} from "forge-std/console.sol";
 import {Executor} from "../src/Executor.sol";
 import {ERC20Mock} from "../lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
 //import {DeployPermit2 as DeployPermit2Script} from "../lib/permit2/script/DeployPermit2.s.sol";
-import {DeployPermit2 as DeployPermit2Helper} from "../lib/permit2/test/utils/DeployPermit2.sol";
+//import {DeployPermit2 as DeployPermit2Helper} from "../lib/permit2/test/utils/DeployPermit2.sol";
 
 contract DeployExecutorLocal is Script {
+    //address constant PERMIT2_ADDRESS = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+    address constant PERMIT2_ADDRESS = 0x04Bf6dd19092d8DC89dE2499a037958296D20E49;
+
     function run() external returns (Executor) {
-        // Deploy Permit2
-        //Permit2 permit2 = new DeployPermit2Script().run();
-        //address permit2Address = address(permit2);
-
-        address permit2Address = new DeployPermit2Helper().deployPermit2();
-
         uint256 pk = vm.envUint("LOCAL_DEPLOYER_KEY");
         address deployer = vm.addr(pk);
 
         vm.startBroadcast(pk);
 
         // Deploy Executor with Permit2 address
-        Executor executor = new Executor(permit2Address, deployer);
+        Executor executor = new Executor(PERMIT2_ADDRESS, deployer);
 
         // Deploy mock tokens
         ERC20Mock tokenA = new ERC20Mock();
@@ -35,12 +32,8 @@ contract DeployExecutorLocal is Script {
         console.log(deployer);
         console.log();
 
-        console.log("PERMIT2 ADDRESS:");
-        console.log(permit2Address);
-        console.log();
-
-        console.log("EXECUTOR ADDRESS:");
-        console.log(address(executor));
+        console.log("OWNER:");
+        console.log(executor.owner());
         console.log();
 
         console.log("TOKEN A ADDRESS:");
@@ -51,9 +44,17 @@ contract DeployExecutorLocal is Script {
         console.log(address(tokenB));
         console.log();
 
-        console.log("OWNER:");
-        console.log(executor.owner());
+        console.log("PERMIT2 ADDRESS:");
+        console.log(PERMIT2_ADDRESS);
         console.log();
+
+        console.log("EXECUTOR ADDRESS:");
+        console.log(address(executor));
+        console.log();
+
+
+
+
 
         return executor;
     }
