5eb376fc25ed226322e472366cd81af4f7e303e1
woutersls <phaerocode@hotmail.com>
2025-10-06 21:05:10 +0200

    chore: initial trader implementation

--- src/traders/UniswapV2Trader.sol
+++ src/traders/UniswapV2Trader.sol
@@ -2,59 +2,54 @@
 pragma solidity ^0.8.20;
 
 import {ITrader} from "../interfaces/ITrader.sol";
-import {IUniswapV2Router02} from "../interfaces/IUniswapV2Router.sol";
+import {IUniswapV2Router02} from "../../lib/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import {IERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ExecutorValidation} from "../libraries/ExecutorValidation.sol";
 
-contract UniswapV2Trader {
-    address immutable EXECUTOR_ADDRESS;
-    address public constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D; // Mainnet router
+contract UniswapV2Trader is ITrader {
+    address immutable executorAddress;
+    address immutable uniswapV2RouterAddress;
+    IUniswapV2Router02 immutable uniV2Router;
+
+    string public constant NAME = "UNISWAP V2 Trader";
 
     error NotCalledByExecutorError();
     error UnknownTradeTypeError();
 
-    constructor(address _EXECUTOR_ADDRESS) {
-        EXECUTOR_ADDRESS = _EXECUTOR_ADDRESS;
+    constructor(address _executorAddress, address _uniswapV2RouterAddress) {
+        executorAddress = _executorAddress;
+        uniswapV2RouterAddress = _uniswapV2RouterAddress;
+        uniV2Router = IUniswapV2Router02(_uniswapV2RouterAddress);
     }
 
     modifier onlyExecutor() {
-        if (msg.sender != EXECUTOR_ADDRESS) revert NotCalledByExecutorError();
+        if (msg.sender != executorAddress) revert NotCalledByExecutorError();
         _;
     }
 
-    function trade(ITrader.TradeParameters calldata params) external onlyExecutor {
-        address to = EXECUTOR_ADDRESS;
+    function trade(ITrader.TradeParameters calldata params) external onlyExecutor returns (uint256) {
+        address to = executorAddress;
         uint256 deadline = params.expiry;
-        
+        address[] calldata path = params.routeData.path;
+        uint256 amountOutMin = params.amountOutMin;
         uint256 amountIn = params.amountIn;
 
+        uint256 amountOut = 0;
         if (params.tradeType == ExecutorValidation.TradeType.ETH_INPUT_TOKEN_OUTPUT) {
-            uint[] memory amounts = IUniswapV2Router02(UNISWAP_V2_ROUTER).swapExactETHForTokens{value: params.amountIn}(
-                params.amountOutMin,
-                params.routeData.path,
-                to,
-                deadline
-            );
+            uint256[] memory amounts =
+                uniV2Router.swapExactETHForTokens{value: params.amountIn}(amountOutMin, path, to, deadline);
+            amountOut = amounts[amounts.length - 1];
         } else if (params.tradeType == ExecutorValidation.TradeType.TOKEN_INPUT_ETH_OUTPUT) {
-            IERC20(params.tokenIn).approve(UNISWAP_V2_ROUTER, params.amountIn);
-            uint[] memory amounts = IUniswapV2Router02(UNISWAP_V2_ROUTER).swapExactTokensForETH(
-                params.amountIn,
-                params.amountOutMin,
-                params.routeData.path,
-                to,
-                deadline
-            );
+            IERC20(params.routeData.path[0]).approve(uniswapV2RouterAddress, params.amountIn);
+            uint256[] memory amounts = uniV2Router.swapExactTokensForETH(amountIn, amountOutMin, path, to, deadline);
+            amountOut = amounts[amounts.length - 1];
         } else if (params.tradeType == ExecutorValidation.TradeType.TOKEN_INPUT_TOKEN_OUTPUT) {
-            IERC20(params.tokenIn).approve(UNISWAP_V2_ROUTER, params.amountIn);
-            uint[] memory amounts = IUniswapV2Router02(UNISWAP_V2_ROUTER).swapExactTokensForTokens(
-                params.amountIn,
-                params.amountOutMin,
-                params.routeData.path,
-                to,
-                deadline
-            );
+            IERC20(params.routeData.path[0]).approve(uniswapV2RouterAddress, params.amountIn);
+            uint256[] memory amounts = uniV2Router.swapExactTokensForTokens(amountIn, amountOutMin, path, to, deadline);
+            amountOut = amounts[amounts.length - 1];
         } else {
             revert UnknownTradeTypeError();
         }
+        return amountOut;
     }
 }
--- src/traders/UniswapV3Trader.sol
+++ src/traders/UniswapV3Trader.sol
@@ -2,7 +2,69 @@
 pragma solidity ^0.8.20;
 
 import {ITrader} from "../interfaces/ITrader.sol";
+import {ISwapRouter} from "../../lib/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
-contract UniswapV3Trader {
+contract UniswapV3Trader is ITrader {
+    address immutable executorAddress;
+    address immutable uniswapV3RouterAddress;
+    ISwapRouter immutable uniV3Router;
 
+    string public constant NAME = "UNISWAP V3 Trader";
+
+    error NotCalledByExecutorError();
+    error UnknownTradeTypeError();
+
+    constructor(address _executorAddress, address _uniswapV3RouterAddress) {
+        executorAddress = _executorAddress;
+        uniswapV3RouterAddress = _uniswapV3RouterAddress;
+        uniV3Router = ISwapRouter(_uniswapV3RouterAddress);
+    }
+
+    modifier onlyExecutor() {
+        if (msg.sender != executorAddress) revert NotCalledByExecutorError();
+        _;
+    }
+
+    function trade(ITrader.TradeParameters calldata params) external onlyExecutor returns (uint256) {
+        bool isMultiHop = (params.routeData.path.length > 2);
+        address recipient = executorAddress;
+        uint256 deadline = params.expiry;
+        uint256 amountIn = params.amountIn;
+        uint256 amountOutMin = params.amountOutMin;
+
+        if (!isMultiHop) {
+            ISwapRouter.ExactInputSingleParams memory exactInputSingle = ISwapRouter.ExactInputSingleParams({
+                tokenIn: params.routeData.path[0],
+                tokenOut: params.routeData.path[1],
+                fee: params.routeData.fee[0],
+                recipient: recipient,
+                deadline: deadline,
+                amountIn: amountIn,
+                amountOutMinimum: amountOutMin,
+                sqrtPriceLimitX96: 0
+            });
+            return uniV3Router.exactInputSingle(exactInputSingle);
+        } else {
+            bytes memory encodedPath = _createEncodedPath(params.routeData.path, params.routeData.fee);
+            ISwapRouter.ExactInputParams memory exactInput = ISwapRouter.ExactInputParams({
+                path: encodedPath,
+                recipient: recipient,
+                deadline: deadline,
+                amountIn: amountIn,
+                amountOutMinimum: amountOutMin
+            });
+            return uniV3Router.exactInput(exactInput);
+        }
+    }
+
+    function _createEncodedPath(address[] memory path, uint24[] memory fees) internal pure returns (bytes memory) {
+        bytes memory encodedPath;
+
+        for (uint256 i = 0; i < fees.length; i++) {
+            encodedPath = abi.encodePacked(encodedPath, path[i], fees[i]);
+        }
+        encodedPath = abi.encodePacked(encodedPath, path[path.length - 1]);
+
+        return encodedPath;
+    }
 }
